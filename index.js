d52dfdd8725b9c3d27beb5ae4d1d0f1e6e8b4a62
Brad Bird <brad@128bits.co.uk>
2019-07-26 17:23:29 +0100

    wip

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const babel = require("@babel/register")
 const personaEngine = require('./test-asset-generator/engines/persona-engine')
 const common = require('./lib/common')
 const override = require('./lib/vmd')
