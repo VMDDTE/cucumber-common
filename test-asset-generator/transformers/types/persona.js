7f18b5976780a25acbdf10da7e630d799cb2b922
Phil Glen <philglen@gmail.com>
2019-07-28 17:28:35 +0100

    SIS-3726: add persona transformer

--- test-asset-generator/transformers/types/persona.js
+++ test-asset-generator/transformers/types/persona.js
@@ -4,7 +4,21 @@ export default class Persona {
     }
 
     transform() {
-        // Do transform stuff here.
-        return this.data;
+        const data = {
+            type: "Person",
+            data: {
+                givenName : this.data.givenName,
+                familyName: this.data.familyName,
+                additionalName: this.data.additionalName,
+                gender: this.data.gender,
+                email: this.data.email,
+                birthDate: this.data.birthDate,
+                jobTitle: this.data.jobTitle,
+                RCVSNo: this.data.RCVSNo,
+                // Not sure if this should be a map over the array?
+                worksFor: this.data.worksFor
+            }
+        }
+        return data;
     }
 }
