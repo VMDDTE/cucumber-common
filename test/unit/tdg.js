d816c3084a934881c9261b45deba07aa5f563ef7
Mark Birbeck <mark.birbeck@gmail.com>
2019-07-30 15:32:16 +0100

    SIS-3717: Remove top-level main() since using test suite

--- test/unit/tdg.js
+++ test/unit/tdg.js
@@ -1,69 +1,65 @@
 const tap = require('tap')
 const generator = require('data-generator')
 
-const main = async () => {
-    tap.test('rnr', async t => {
-        const dataKey = 'rnr'
-
-        /**
-       * Clear the test data:
-       */
-        generator.clearAll()
-
-        /**
-       * Load the R&R file:
-       */
-        await generator.generate(dataKey)
-
-        /**
-       * Check that there is a user, and that the email address
-       * has the correct format:
-       */
-        const testuser = await generator.getTestUser(dataKey)
-
-        t.ok(testuser)
-        t.ok(testuser.Email)
-        t.match(testuser.Email, /\d{10}.vmd.vet.test@gmail.com$/)
-
-        /**
-       * Tear down properly:
-       */
-        await generator.tearDown(dataKey)
-    })
+tap.test('rnr', async t => {
+    const dataKey = 'rnr'
 
     /**
-     * Now test Mary:
-     */
-    tap.test('mary', async t => {
-        const dataKey = 'mary'
-
-        generator.clearAll()
-        await generator.generate(dataKey)
-        const testuser = await generator.getTestUser(dataKey)
-
-        t.ok(testuser)
-        t.ok(testuser.Email)
-        t.match(testuser.Email, /^em@il.me$/)
-
-        await generator.tearDown(dataKey)
-    })
+   * Clear the test data:
+   */
+    generator.clearAll()
 
     /**
-     * Now test Robert:
-     */
-    tap.test('robert', async t => {
-        const dataKey = 'robert'
+   * Load the R&R file:
+   */
+    await generator.generate(dataKey)
 
-        generator.clearAll()
-        await generator.generate(dataKey)
-        const testuser = await generator.getTestUser(dataKey)
+    /**
+   * Check that there is a user, and that the email address
+   * has the correct format:
+   */
+    const testuser = await generator.getTestUser(dataKey)
 
-        t.ok(testuser)
-        t.ok(testuser.Email)
-        t.match(testuser.Email, /^robert.price@rnr.com$/)
+    t.ok(testuser)
+    t.ok(testuser.Email)
+    t.match(testuser.Email, /\d{10}.vmd.vet.test@gmail.com$/)
 
-        await generator.tearDown(dataKey)
-    })
-}
+    /**
+   * Tear down properly:
+   */
+    await generator.tearDown(dataKey)
+})
 
-main()
+/**
+ * Now test Mary:
+ */
+tap.test('mary', async t => {
+    const dataKey = 'mary'
+
+    generator.clearAll()
+    await generator.generate(dataKey)
+    const testuser = await generator.getTestUser(dataKey)
+
+    t.ok(testuser)
+    t.ok(testuser.Email)
+    t.match(testuser.Email, /^em@il.me$/)
+
+    await generator.tearDown(dataKey)
+})
+
+/**
+ * Now test Robert:
+ */
+tap.test('robert', async t => {
+    const dataKey = 'robert'
+
+    generator.clearAll()
+    await generator.generate(dataKey)
+    const testuser = await generator.getTestUser(dataKey)
+
+    t.ok(testuser)
+    t.ok(testuser.Email)
+    t.match(testuser.Email, /^robert.price@rnr.com$/)
+
+    await generator.tearDown(dataKey)
+})
