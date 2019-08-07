a1ae3d54e6aa7a5a78510225258e3f02cd83f7d4
richard <richard@ifocs.com>
2019-08-07 13:34:40 +0100

    Fix logic

--- test-asset-generator/transformers/transformer.js
+++ test-asset-generator/transformers/transformer.js
@@ -30,9 +30,10 @@ class Transformer {
         }
     }
 
-    createOrganisation(org) {
+    createOrganisation(orgName) {
         try {
-            const transformedOrganisation = (new OrganisationTransformer(org, this.namespace)).transform();
+            const organisation = OrganisationFactory(name)
+            const transformedOrganisation = (new OrganisationTransformer(organisation, this.namespace)).transform();
             return this.addMetaDataToAsset(TDG_CONSTANTS.ACTION_CREATE, transformedOrganisation, index)
             
         } catch (err) {
@@ -40,7 +41,7 @@ class Transformer {
         }
     }
     
-    createRole(role) {
+    createRole(roleName) {
         try {
             const transformedRole = (new RoleTransformer(role, this.namespace)).transform()
             return this.addMetaDataToAsset(TDG_CONSTANTS.ACTION_ASSIGN_ROLE, transformedRole, index)
@@ -66,7 +67,7 @@ class Transformer {
         const maArray = []
 
         testUser.Roles.forEach(({ name, role }, index) => {
-            const organisationTDG = OrganisationFactory(name)
+            const organisationTDG = this.createOrganisation(name)
             const roleTDG = this.createRole(role)
 
             orgArray.push(this.createOrganisation(organisationTDG));
@@ -97,7 +98,6 @@ class Transformer {
         return asset;
     }
 }
-
 
 /**
  * transformer is a small factory function to give us control over the use of the
