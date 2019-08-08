c9e78dafa307718a31707db852ae3c0ef9e00930
foxleigh81 <alex@alexfoxleigh.com>
2019-08-08 11:02:46 +0100

    updated test assets to accomodate test users and orgs

--- test-asset-generator/assets/organisations/index.js
+++ test-asset-generator/assets/organisations/index.js
@@ -1,7 +1,9 @@
 const org1 = require('./pterodactyl-laboratories/organisation.json')
 const org2 = require('./raptors-and-reptiles/organisation.json')
+const testOrg = require('./test-organisation/organisation.json')
 
 module.exports = [
     org1,
-    org2
+    org2,
+    testOrg
 ]
--- test-asset-generator/assets/personae/index.js
+++ test-asset-generator/assets/personae/index.js
@@ -1,4 +1,6 @@
 module.exports = [
     require('./mary.json'),
-    require('./robert.json')
+    require('./robert.json'),
+    // Do not delete or modify the test user
+    require('./test-user.json')
 ]
