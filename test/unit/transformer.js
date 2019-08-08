071251e8eee3ba7067b116fe6d6c2a39af40969e
foxleigh81 <alex@alexfoxleigh.com>
2019-08-08 10:14:30 +0100

    fixed 'throws' error

--- test/unit/transformer.js
+++ test/unit/transformer.js
@@ -2,16 +2,18 @@ const tap = require('tap')
 
 const transformer = require('../../test-asset-generator/transformers/transformer')
 
+const testUser = transformer('Robert Price', 'namespace')
+
+
 tap.test('Check the persona is being returned', t => {
-  const testData = transformer('Robert Price', 'namespace')
-  t.equal(testData[2].data.name, 'Robert Price')
-  t.equal(testData[2].data.email, '{usergen}')
-  t.equal(testData[2].action, 'create')
-  t.equal(testData[2].label, 'ExternalUserRecord0')
+  t.equal(testUser[2].data.name, 'Robert Price')
+  t.equal(testUser[2].data.email, '{usergen}')
+  t.equal(testUser[2].action, 'create')
+  t.equal(testUser[2].label, 'ExternalUserRecord0')
   t.end()
 })
 
 tap.test('Check that an error is thrown if no persona', t => {
-  t.throws(transformer('No User', 'namespace'), new Error('No assets have been provided'), 'Should throw error')
+  t.throws(() => transformer('No User', 'namespace'), new Error('No assets have been provided'), 'Should throw error')
   t.end()
 })
